9785087a02477f1254a2a97d8eaf914b892cd129
aleguzman88 <108032026+aleguzman88@users.noreply.github.com>
2023-02-07 14:56:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const app = express();
+import express from 'express';
+var app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from App Engine!');
+app.use('/', function(req, res) {
+res.send('Hello World');
 });
 
-// Listen to the App Engine-specified port, or 8080 otherwise
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
+if (require.main === app.js) {
+app.listen(3000, function () {
+console.log('Server running at http://localhost:3000/');
 });
+} else {
+module.exports = app;
+}
