c8de5e6fa3ba9b2aff93bda64310f54df47ab242
manuel guzman <aleguzman88@gmail.com>
2023-03-02 23:43:19 -0400

    update

---
+++ app.js
@@ -0,0 +1,63 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// views engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Adding the Home route
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Home' });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about/about', { title: 'About' });
+});
+app.get('/projects', (req, res) => {
+  res.render('projects/projects', { title: 'Projects' });
+
+});
+
+app.get('/services', (req, res) => {
+  res.render('services/services', { title: 'Services' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact/contact', { title: 'Contact' });
+});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+
